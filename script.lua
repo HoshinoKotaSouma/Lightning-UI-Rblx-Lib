fd3b32036bbdeccc5009cfbc88c314fa8cf97710
@BaconnRinn <kiluane12@gmail.com>
2025-12-10 20:20:00 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1952,7 +1952,7 @@ local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoshin
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 local Window = Fluent:CreateWindow({
-    Title = " Sybau [Beta Version]" .. Fluent.Version,
+    Title = " Fleurdelys Scripts [Beta Version]" .. Fluent.Version,
     SubTitle = "by Yue1lit",
     Search = true, -- optional and default true
     Icon = "rbxassetid://83022373580647", -- optional
@@ -2035,7 +2035,7 @@ local Tabs = {
     })
 }
 local Minimizer = Fluent:CreateMinimizer({
-  Icon = "rbxassetid://78469182446595", -- you can put AssetId
+  Icon = "rbxassetid://82069257600944", -- you can put AssetId
   Size = UDim2.fromOffset(44, 44),
   Position = UDim2.new(0, 320, 0, 24),
   Acrylic = true,
